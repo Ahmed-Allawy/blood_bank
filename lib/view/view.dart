fb0f1b9ef1f73470551140a961994f09bd5e6d87
Ahmed-Allawy <M_ahmed20156957@yahoo.com>
2023-03-25 09:39:07 +0200

    first commit

---
+++ lib/view/view.dart
@@ -0,0 +1,7 @@
+
+// this file we delete :)
+/*
+blood bank project
+=> architecture : we will use mvvm architecture
+=> state management : we will use cubit 
+*/  
---
+++ lib/view_model/view_model.dart
@@ -0,0 +1,6 @@
+// this file we delete :)
+/*
+blood bank project
+=> architecture : we will use mvvm architecture
+=> state management : we will use cubit 
+*/  
