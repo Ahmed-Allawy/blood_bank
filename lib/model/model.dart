4a05e932eaa5356e463dc277caac0694d3f05099
Ahmed Alaa <ahmedalaa1316@gmail.com>
2023-05-11 03:28:15 +0300

    model change and http

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,7 +1,82 @@
+import 'dart:convert';
 
-// this file we delete :)
-/*
-blood bank project
-=> architecture : we will use mvvm architecture
-=> state management : we will use cubit 
-*/  
+//  this is the jason form the user {
+//     "email": "",
+//     "password": "",
+//     "Fname": "",
+//     "DateOfBirth": null,
+//     "phone_num": "",
+//     "location": "",
+//     "blood_group": null
+// }
+class User {
+  String email;
+  String password;
+  String Fname;
+  String DateOfBirth;
+  int phone_num;
+  String location;
+  String blood_group;
+  User({
+    required this.email,
+    required this.password,
+    required this.Fname,
+    required this.DateOfBirth,
+    required this.phone_num,
+    required this.location,
+    required this.blood_group,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'email': email,
+      'password': password,
+      'Fname': Fname,
+      'DateOfBirth': DateOfBirth,
+      'phone_num': phone_num,
+      'location': location,
+      'blood_group': blood_group,
+    };
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      email: map['email'] as String,
+      password: map['password'] as String,
+      Fname: map['Fname'] as String,
+      DateOfBirth: map['DateOfBirth'] as String,
+      phone_num: map['phone_num'] as int,
+      location: map['location'] as String,
+      blood_group: map['blood_group'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory User.fromJson(String source) =>
+      User.fromMap(json.decode(source) as Map<String, dynamic>);
+}
+//blood request self / other response 
+// {
+//     "id": 14,
+//     "user": "0123456789",
+//     "Fname": "Allawy",
+//     "location": "Faqous",
+//     "approved": false,
+//     "blood_group": null,
+//     "date": "2023-05-11",
+//     "time": "00:26:07.701335"
+// }
+
+//blood donate self/other response 
+// {
+//     "id": 3,
+//     "user": "0123456789",
+//     "Fname": "Allawy",
+//     "location": "Faqous",
+//     "approved": false,
+//     "blood_group": null,
+//     "date": "2023-05-11",
+//     "time": "00:24:49.971764"
+// }
+
--- lib/view/presentation/Blood_Request/Blood_request_view.dart
+++ lib/view/presentation/Blood_Request/Blood_request_view.dart
@@ -2,11 +2,14 @@
 // ignore: file_names
 // ignore_for_file: file_names, avoid_print
 
+import 'dart:convert';
+
 import 'package:blood_bank/view/shared/component/components.dart';
 import 'package:flutter/material.dart';
-
+import '../../../model/model.dart';
 import '../../shared/component/helperfunctions.dart';
 import '../home_screen/home_body.dart';
+import 'package:http/http.dart' as http;
 
 class BloodRequestView extends StatefulWidget {
   const BloodRequestView({super.key});
@@ -28,6 +31,9 @@ class _BloodrequestViewState extends State<BloodRequestView> {
       doneOntap: () {
         setState(() {
           print("object");
+          // sendBloodRequest();
+          // signup(user);
+          login();
         });
       },
       pageTitle: 'Blood Blood',
@@ -37,5 +43,72 @@ class _BloodrequestViewState extends State<BloodRequestView> {
       imageBlood: 'assets/O.png',
       personName: 'Ahmed allawy',
     );
+  }
+}
+
+void sendBloodRequest() async {
+  var headers = {
+    'Authorization': 'Token a5fd5ff9e485f4938c0bd3606c1d5060c7e63821'
+  };
+  var request = http.Request(
+      'POST', Uri.parse('http://127.0.0.1:8000/blood/request-self/'));
+
+  request.headers.addAll(headers);
+
+  http.StreamedResponse response = await request.send();
+
+  if (response.statusCode == 200) {
+    print(await response.stream.bytesToString());
+  } else {
+    print(response.reasonPhrase);
+  }
+}
+
+void signup(User user) async {
+  var headers = {'Content-Type': 'application/json'};
+  var request =
+      http.Request('POST', Uri.parse('http://127.0.0.1:8000/singup/'));
+  request.body = user.toJson();
+  request.headers.addAll(headers);
+
+  http.StreamedResponse response = await request.send();
+
+  if (response.statusCode == 200) {
+    print(await response.stream.bytesToString());
+  } else {
+    print(response.reasonPhrase);
+  }
+}
+
+// this is the user entery
+var user = User(
+  email: 'example@example.com',
+  password: 'password',
+  Fname: 'John Doe',
+  DateOfBirth: '1990-01-01',
+  phone_num: 123456789,
+  location: 'New York',
+  blood_group: 'O+',
+);
+
+//login function
+void login() async {
+  var headers = {'Content-Type': 'application/json'};
+  var request = http.Request('POST', Uri.parse('http://127.0.0.1:8000/login/'));
+  //here I send the password and the phone number for login by passing them to the body
+  request.body = json.encode({"password": "123", "phone_num": "01154363771"});
+  request.headers.addAll(headers);
+
+  http.StreamedResponse response = await request.send();
+  //here is the response
+  if (response.statusCode == 200) {
+    String body = await response.stream.bytesToString();
+    var responseJason = jsonDecode(body);
+    //here I save the token to var you can save it to sharedprefrence
+    var token = responseJason['token'];
+    print(body);
+    print("Token$token");
+  } else {
+    print(response.reasonPhrase);
   }
 }
